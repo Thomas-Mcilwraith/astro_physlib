f14363075cc7563a95b34d51fb794b8d941eb89b
Thomas-Mcilwraith <thomas_mcilwraith@outlook.com>
2026-04-16 22:22:34 +0100

    feat!: initial commit

---
+++ src/c/orbit-dynamics/test-program/test.c
@@ -0,0 +1,13 @@
+/*
+ * test.c
+ *
+ *  Created on: Jul 10, 2017
+ *      Author: tmcilwaith
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Hello World!\n");
+    return 0;
+}
---
+++ src/c/orbit-dynamics/test-program/test.h
@@ -0,0 +1,11 @@
+/*
+ * test.h
+ *
+ *  Created on: Jul 10, 2017
+ *      Author: tmcilwaith
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+#endif /* TEST_H_ */
